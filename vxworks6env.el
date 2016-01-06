042a8c7db7d8ee765ae0f0208f28a7b13544b13b
Ed Liversidge <ed@harmonicss.co.uk>
2016-01-06 11:05:38 +0000

    First commit v0.1

---
+++ vxworks6env.el
@@ -0,0 +1,139 @@
+;;
+;; Setups up the paths for a vxWorks 6 installation. 
+;;
+;; requires code from vxworks.el
+;;
+;;
+
+(setenv "COMP_CAFE" "cafe-0.1")
+
+(setenv "COMP_CAFE_PATH" "C:/WindRiver_vxw6.9.2.2/components/cafe-0.1")
+
+(setenv "COMP_CAN" "can-1.5")
+
+(setenv "COMP_DCOM" "dcom-2.3")
+
+(setenv "COMP_IPNET2" "ip_net2-6.9")
+
+(setenv "COMP_IPNET2_SUBCOMP" "ip_net2-6.9/ipmcp/config;ip_net2-6.9/wrnad/config;ip_net2-6.9/vxifproxy/config;ip_net2-6.9/ipcavium/config;ip_net2-6.9/ipfreescale/config;ip_net2-6.9/iphwcrypto/config;ip_net2-6.9/ipipsec2/config;ip_net2-6.9/ipfirewall/config;ip_net2-6.9/ipmpls/config;ip_net2-6.9/ipmip6mn/config;ip_net2-6.9/ipmip6pm/config;ip_net2-6.9/ipmip6ha/config;ip_net2-6.9/ipmip6/config;ip_net2-6.9/ipmipfa/config;ip_net2-6.9/ipmippm/config;ip_net2-6.9/ipmipmn/config;ip_net2-6.9/ipmipha/config;ip_net2-6.9/ipmip4/config;ip_net2-6.9/ipmip/config;ip_net2-6.9/iptcp/config;ip_net2-6.9/ipike/config;ip_net2-6.9/ipradius/config;ip_net2-6.9/ipl2tp/config;ip_net2-6.9/ipppp/config;ip_net2-6.9/iplite2/config;ip_net2-6.9/vxcoreip/config;ip_net2-6.9/vxcompat/config;ip_net2-6.9/ipssl2/config;ip_net2-6.9/ipssh/config;ip_net2-6.9/ipcrypto/config;ip_net2-6.9/ipnet2/config;ip_net2-6.9/ipcom/config;ip_net2-6.9/vxbridge/config;ip_net2-6.9/iprohc/config;ip_net2-6.9/ipdhcpc6/config;ip_net2-6.9/ipdhcpr/config;ip_net2-6.9/ipripng/config;ip_net2-6.9/iprip/config;ip_net2-6.9/ipntp/config;ip_net2-6.9/ipsntp/config;ip_net2-6.9/ipappl/config;ip_net2-6.9/ipsctp/config;ip_net2-6.9/ipdhcps6/config;ip_net2-6.9/ipdhcps/config;ip_net2-6.9/ipdiameter/config;ip_net2-6.9/ipwlan/config;ip_net2-6.9/ipmacsec/config;ip_net2-6.9/ip8021x/config;ip_net2-6.9/vxdot1x/config;ip_net2-6.9/ipwps/config;ip_net2-6.9/ipeap/config")
+
+(setenv "COMP_MIPC" "mipc-2.1")
+
+(setenv "COMP_NETSTACK" "netstack-6.9")
+
+(setenv "COMP_OPC" "opc-3.2")
+
+(setenv "COMP_SNMP" "wrsnmp-10.5")
+
+(setenv "COMP_STACKTRACE" "stacktrace-1.0")
+
+(setenv "COMP_STACKTRACE_PATH" "C:/WindRiver_vxw6.9.2.2/components/stacktrace-1.0")
+
+(setenv "COMP_SUBCOMP_LIST" "COMP_IPNET2_SUBCOMP")
+
+(setenv "COMP_TCF_AGENT" "tcf_agent-0.3")
+
+(setenv "COMP_TCF_AGENT_PATH" "C:/WindRiver_vxw6.9.2.2/components/tcf_agent-0.3")
+
+(setenv "COMP_WEBCLI" "webcli-4.9")
+
+(setenv "COMP_WEBSERVICES" "webservices-1.7")
+
+(setenv "COMP_WINDML" "windml-5.3")
+
+(setenv "COMP_WLAN" "wlan-3.3")
+
+(setenv "COMP_WRLOAD" "wrload-1.0")
+
+(setenv "DFW_PLUGIN_PATH" "C:/WindRiver_vxw6.9.2.2/ocd-3.3")
+
+(setenv "DOCS_ROOT" "C:/WindRiver_vxw6.9.2.2/docs/extentions/eclipse/plugins")
+
+(setenv "FLEXLM_NO_CKOUT_INSTALL_LIC" "1")
+
+(setenv "IA32ROOT" "C:/WindRiver_vxw6.9.2.2/intel/20110124-vxworks-6.9")
+
+(setenv "LD_LIBRARY_PATH" "C:/WindRiver_vxw6.9.2.2/vxworks-6.9/host/x86-win32/lib;C:/WindRiver_vxw6.9.2.2/workbench-3.3/foundation/x86-win32/lib;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrsysviewer/host/x86-win32/lib;C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis/host/bin/x86-win32;C:/WindRiver_vxw6.9.2.2/workbench-3.3/x86-win32/lib;C:/WindRiver_vxw6.9.2.2/lmapi-5.0/x86-win32/lib;C:/WindRiver_vxw6.9.2.2/intel/20110124-vxworks-6.9/x86-win32/bin/ia32")
+
+(setenv "LM_A_APP_DISABLE_CACHE_READ" "set")
+
+(setenv "MANPATH" "C:/WindRiver_vxw6.9.2.2/vxworks-6.9/man")
+
+(setenv "PATH" "C:/WindRiver_vxw6.9.2.2/components/windml-5.3/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/components/webservices-1.7/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/components/wrll-wrsnmp/packages/wrsnmp-10.3/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/components/webcli-4.9/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/components/opc-3.2/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/components/ip_net2-6.9/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/components/dcom-2.3/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/vxworks-6.9/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/vxworks-6.9/vxtest/src/scripts;C:/WindRiver_vxw6.9.2.2/vxworks-6.9/host/binutils/x86-win32/bin;C:/WindRiver_vxw6.9.2.2;C:/WindRiver_vxw6.9.2.2/utilities-1.0/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/ocd-3.3/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/workbench-3.3/foundation/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrsysviewer/host/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/platform/x86-win32/eclipse;C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis/host/bin/x86-win32;C:/WindRiver_vxw6.9.2.2/workbench-3.3/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/lmapi-5.0/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/gnu/4.3.3-vxworks-6.9/x86-win32/bin;C:/WindRiver_vxw6.9.2.2/intel/20110124-vxworks-6.9/x86-win32/bin/ia32;C:/WindRiver_vxw6.9.2.2/diab/5.9.1.0/WIN32/bin;C:/Program Files/Common Files/Microsoft Shared/Windows Live;C:/cygwin64/bin;C:/WINDOWS/system32;C:/WINDOWS;C:/WINDOWS/System32/Wbem;C:/WINDOWS/System32/WindowsPowerShell/v1.0/;c:/Program Files (x86)/ATI Technologies/ATI.ACE/Core-Static;C:/Program Files (x86)/Windows Live/Shared;C:/Program Files (x86)/Common Files/Roxio Shared/DLLShared/;C:/Program Files (x86)/Common Files/Roxio Shared/OEM/DLLShared/;C:/Program Files (x86)/Common Files/Roxio Shared/OEM/12.0/DLLShared/;C:/Program Files (x86)/Roxio/OEM/AudioCore/;C:/Program Files/TortoiseSVN/bin;C:/Program Files/Microsoft SQL Server/110/Tools/Binn/;C:/Program Files/Microsoft Windows Performance Toolkit/;C:/Program Files/OpenVPN/bin;C:/Program Files (x86)/Graphviz2.38/bin;C:/Program Files (x86)/VisualSVN Server/bin;C:/Program Files (x86)/Skype/Phone/;C:/Program Files (x86)/Gnu Global 651/bin;")
+
+(setenv "TCLLIBPATH" "C:/WindRiver_vxw6.9.2.2/vxworks-6.9/host/resource/tcl")
+
+(setenv "WIND_ANALYSIS_TARGET_SCRIPT_BASE" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis/target/scripts")
+
+(setenv "WIND_BASE" "C:/WindRiver_vxw6.9.2.2/vxworks-6.9")
+
+(setenv "WIND_COMPONENTS" "C:/WindRiver_vxw6.9.2.2/components")
+
+(setenv "WIND_COMPONENTS_INCLUDES" "C:/WindRiver_vxw6.9.2.2/components/windml-5.3/h;C:/WindRiver_vxw6.9.2.2/components/webservices-1.7/h;C:/WindRiver_vxw6.9.2.2/components/webcli-4.9/target/h;C:/WindRiver_vxw6.9.2.2/components/tcf_agent-0.3/agent;C:/WindRiver_vxw6.9.2.2/components/stacktrace-1.0/include;C:/WindRiver_vxw6.9.2.2/components/opc-3.2/h;C:/WindRiver_vxw6.9.2.2/components/dcom-2.3/h;C:/WindRiver_vxw6.9.2.2/components/cafe-0.1/hpc/include;C:/WindRiver_vxw6.9.2.2/components/cafe-0.1/cafe/include;C:/WindRiver_vxw6.9.2.2/components/cafe-0.1/vxworks/include")
+
+(setenv "WIND_COMPONENTS_LIBNAMES" "windml-5.3_dyn;windml-5.3;webservices-1.7;webcli-4.9;tcf_agent-0.3;stacktrace-1.0;opc-3.2;ip_net2-6.9;dcom-2.3;cafe-0.1")
+
+(setenv "WIND_COMPONENTS_LIBPATHS" "C:/WindRiver_vxw6.9.2.2/components/obj/vxworks-6.9/krnl/lib")
+
+(setenv "WIND_DFW_PATH" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/dfw/")
+
+(setenv "WIND_DIAB_PATH" "C:/WindRiver_vxw6.9.2.2/diab/5.9.1.0")
+
+(setenv "WIND_DOCS" "C:/WindRiver_vxw6.9.2.2/docs")
+
+(setenv "WIND_EXTENSIONS" "C:/WindRiver_vxw6.9.2.2/components/windml-5.3/extensions;C:/WindRiver_vxw6.9.2.2/components/webservices-1.7/extensions;C:/WindRiver_vxw6.9.2.2/components/webcli-4.9/extensions;C:/WindRiver_vxw6.9.2.2/components/opc-3.2/extensions;C:/WindRiver_vxw6.9.2.2/components/ip_net2-6.9/extensions;C:/WindRiver_vxw6.9.2.2/components/dcom-2.3/extensions;C:/WindRiver_vxw6.9.2.2/components/extensions;C:/WindRiver_vxw6.9.2.2/ocd-3.3;C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis/valgrind;C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis/extensions;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrsysviewer;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/pdt;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/intel;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/webtools;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/wre;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vspworks;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/tools;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/wrlinux;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/wrhv;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vthreads;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vxworksmilshae;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vxworksmils;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vxworkscert;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vxworks653;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vxworks55;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/vxworks;C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/wrworkbench;C:/WindRiver_vxw6.9.2.2/wrmscomponents/diagnostics/extensions;C:/WindRiver_vxw6.9.2.2/wrlinux-2.0/extensions;C:/WindRiver_vxw6.9.2.2/unittester-2.5/extensions;C:/WindRiver_vxw6.9.2.2/unittester-2.4/extensions;C:/WindRiver_vxw6.9.2.2/studio-2.0/extensions;C:/WindRiver_vxw6.9.2.2/components/windml-5.1/extensions;C:/WindRiver_vxw6.9.2.2/components/windml-5.0/extensions;C:/WindRiver_vxw6.9.2.2/components/windml-4.2/extensions;C:/WindRiver_vxw6.9.2.2/components/windml-4.1/extensions;C:/WindRiver_vxw6.9.2.2/components/webservices-1.5/extensions;C:/WindRiver_vxw6.9.2.2/components/webservices-1.4/extensions;C:/WindRiver_vxw6.9.2.2/components/webservices-1.3/extensions;C:/WindRiver_vxw6.9.2.2/components/webcli-4.7/extensions;C:/WindRiver_vxw6.9.2.2/components/opc-3.1/extensions;C:/WindRiver_vxw6.9.2.2/docs/extensions")
+
+(setenv "WIND_FOUNDATION_PATH" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/foundation")
+
+(setenv "WIND_GNU_PATH" "C:/WindRiver_vxw6.9.2.2/gnu/4.3.3-vxworks-6.9")
+
+(setenv "WIND_HOME" "C:/WindRiver_vxw6.9.2.2")
+
+(setenv "WIND_HOST_TYPE" "x86-win32")
+
+(setenv "WIND_ICC_PATH" "C:/WindRiver_vxw6.9.2.2/intel/20110124-vxworks-6.9")
+
+(setenv "WIND_INSTALLER_HOME" "C:/WindRiver_vxw6.9.2.2/maintenance/wrInstaller")
+
+(setenv "WIND_INTRO" "C:/WindRiver_vxw6.9.2.2/vxworks-6.9/gettingStarted/vxworks.properties;C:/WindRiver_vxw6.9.2.2/ocd-3.3/gettingStarted/ocd/ocd.properties;C:/WindRiver_vxw6.9.2.2/workbench-3.3/gettingStarted/wrwb/workbench.properties")
+
+(setenv "WIND_JRE_HOME" "C:/WindRiver_vxw6.9.2.2/jre/1.6.0_21/x86-win32")
+
+(setenv "WIND_OCD_LIBUSB_PATH" "C:/WindRiver_vxw6.9.2.2/ocd-3.3/host/x86-win32/lib")
+
+(setenv "WIND_OCD_PATH" "C:/WindRiver_vxw6.9.2.2/ocd-3.3")
+
+(setenv "WIND_PLATFORM" "vxworks-6.9")
+
+(setenv "WIND_PREFERRED_PACKAGES" "vxworks-6.9;workbench-3.3")
+
+(setenv "WIND_RSS_CHANNELS" "http://www.windriver.com/feeds/vxworks_660.xml;http://www.windriver.com/feeds/workbench_ocd.xml;http://www.windriver.com/feeds/workbench_300.xml")
+
+(setenv "WIND_SAMPLES" "C:/WindRiver_vxw6.9.2.2/components/windml-5.3/samples;C:/WindRiver_vxw6.9.2.2/components/webservices-1.7/tutorials;C:/WindRiver_vxw6.9.2.2/components/webcli-4.9/samples;C:/WindRiver_vxw6.9.2.2/components/opc-3.2/samples;C:/WindRiver_vxw6.9.2.2/components/ip_net2-6.9/samples;C:/WindRiver_vxw6.9.2.2/components/dcom-2.3/samples;C:/WindRiver_vxw6.9.2.2/vxworks-6.9/target/usr/apps/samples;C:/WindRiver_vxw6.9.2.2/vxworks-6.9/target/src/demo;C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis/target/src/linux;C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis/target/src/vxworks;C:/WindRiver_vxw6.9.2.2/workbench-3.3/samples")
+
+(setenv "WIND_SCOPETOOLS_BASE" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/analysis")
+
+(setenv "WIND_TOOLCHAINS" "gnu;icc;diab")
+
+(setenv "WIND_TOOLS" "C:/WindRiver_vxw6.9.2.2/workbench-3.3")
+
+(setenv "WIND_USERMODE_AGENT" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/usermode-agent/bin/usermode-agent.sh")
+
+(setenv "WIND_USERMODE_AGENT_PATH" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/usermode-agent")
+
+(setenv "WIND_USR" "C:/WindRiver_vxw6.9.2.2/vxworks-6.9/target/usr")
+
+(setenv "WIND_UTILITIES" "C:/WindRiver_vxw6.9.2.2/utilities-1.0")
+
+(setenv "WIND_VXCONFIG" "C:/WindRiver_vxw6.9.2.2/components/windml-5.3/osconfig/vxworks;C:/WindRiver_vxw6.9.2.2/components/tcf_agent-0.3/osconfig/vxworks;C:/WindRiver_vxw6.9.2.2/components/stacktrace-1.0/osconfig/vxworks;C:/WindRiver_vxw6.9.2.2/components/ip_net2-6.9/osconfig/vxworks;C:/WindRiver_vxw6.9.2.2/components/cafe-0.1/osconfig/vxworks")
+
+(setenv "WIND_WB_SCRIPTS" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/scripts/")
+
+(setenv "WIND_WRSV_PATH" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrsysviewer")
+
+(setenv "WIND_WRWB_PATH" "C:/WindRiver_vxw6.9.2.2/workbench-3.3/wrwb/platform/x86-win32/eclipse")
+
+(setenv "WRSD_LICENSE_FILE" "C:/WindRiver_vxw6.9.2.2/license")
+
+(setenv "WRVX_COMPBASE" "C:/WindRiver_vxw6.9.2.2/components")
+
